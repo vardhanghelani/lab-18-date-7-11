28bc282805b695af78096fee31a81b48617f2490
Vardhan Ghelani <142500847+vardhanghelani@users.noreply.github.com>
2025-03-21 17:21:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,39 +3,36 @@ const prevBtn = document.querySelector("#prev-btn");
 const nextBtn = document.querySelector("#next-btn");
 const book = document.querySelector("#book");
 
-const papers = Array.from(document.querySelectorAll(".paper"));
+const papers = [
+    document.querySelector("#p1"),
+    document.querySelector("#p2"),
+    document.querySelector("#p3"),
+    document.querySelector("#p4"),
+    document.querySelector("#p5")
+];
 
 // Event Listeners
 prevBtn.addEventListener("click", goPrevPage);
-nextBtn.addEventListener("click", goNextPage");
+nextBtn.addEventListener("click", goNextPage);
 
 // Business Logic
 let currentLocation = 1;
-const numOfPapers = papers.length;
-const maxLocation = numOfPapers + 1;
+let maxLocation = papers.length + 1;
 
 // Functions
 function openBook() {
     book.style.transform = "translateX(50%)";
-    prevBtn.style.transform = "translateX(-180px)";
-    nextBtn.style.transform = "translateX(180px)";
 }
 
 function closeBook(isAtBeginning) {
     book.style.transform = isAtBeginning ? "translateX(0%)" : "translateX(100%)";
-    prevBtn.style.transform = "translateX(0px)";
-    nextBtn.style.transform = "translateX(0px)";
 }
 
 function goNextPage() {
     if (currentLocation < maxLocation) {
-        if (currentLocation === 1) openBook();
-        if (currentLocation <= numOfPapers) {
-            papers[currentLocation - 1].classList.add("flipped");
-            papers[currentLocation - 1].style.zIndex = currentLocation;
-        }
+        papers[currentLocation - 1].classList.add("flipped");
+        papers[currentLocation - 1].style.zIndex = currentLocation;
         currentLocation++;
-        if (currentLocation > numOfPapers) closeBook(false);
     } else {
         resetToFirstPage();
     }
@@ -43,23 +40,21 @@ function goNextPage() {
 
 function goPrevPage() {
     if (currentLocation > 1) {
-        if (currentLocation - 1 === 1) closeBook(true);
-        if (currentLocation - 2 >= 0) {
-            papers[currentLocation - 2].classList.remove("flipped");
-            papers[currentLocation - 2].style.zIndex = numOfPapers - (currentLocation - 2);
-        }
         currentLocation--;
+        papers[currentLocation - 1].classList.remove("flipped");
+        papers[currentLocation - 1].style.zIndex = maxLocation - currentLocation;
     }
 }
 
 function resetToFirstPage() {
     closeBook(true);
     currentLocation = 1;
-    papers.forEach((paper, index) => {
+    papers.forEach((paper) => {
         paper.classList.remove("flipped");
-        paper.style.zIndex = numOfPapers - index;
+        paper.style.zIndex = "0";
     });
+    papers[0].style.zIndex = 4;
 }
 
-// Initialize the book
+// Initialize Book
 resetToFirstPage();
